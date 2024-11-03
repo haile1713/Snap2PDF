d625fdddabf8bab4325a64654645aa885ae30ad6
haile <haileleulfiseha@gmail.com>
2024-11-03 16:38:56 +0300

    installed and edited a library for the canvas

--- app.py
+++ app.py
@@ -36,11 +36,25 @@ class ImageToPDFConverter:
             self.image_paths=filedialog.askopenfilenames(title="Select Images",filetypes=[(" image files ", " *.png *.jpg *.jpeg ")])
             self.update_selected_images_listbox()
         
-        def update_selected_images_listbox():
+        def update_selected_images_listbox(self):
             self.selected_images_listbox.delete(0,tk.END)
+
             for image_path in self.image_paths:
                 _,image_path=os.path.split(image_path)
                 self.selected_images_listbox.insert(tk.END,image_path)
+            
+        def convert_images_to_pdf(self):
+            if not self.image_paths:
+                return
+
+            output_pdf_path=self.output_pdf_name.get()+".pdf"if self.output_pdf_name.get()else "output.pdf"
+
+            pdf=canvas 
+
+
+
+
+
 def main():
     root = tk.Tk()
     root.title("Image to PDF")
